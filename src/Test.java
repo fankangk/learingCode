7ebddc1e4f6719f0c3c5b48183ad78ebfd48e769
fankk <fankk3@chinaunicom.cn>
2018-09-02 22:54:06 +0800

    author:fankk desc:

--- src/Test.java
+++ src/Test.java
@@ -10,9 +10,15 @@ public class Test {
 
     public static void main(String[] args){
         Date d=new Date();
+
         SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("今天的日期："+df.format(d));
-        System.out.println("两天前的日期：" + df.format(new Date(d.getTime() - 2 * 24 * 60 * 60 * 1000)));
-        System.out.println("三天后的日期：" + df.format(new Date(d.getTime() + 40*60 * 1000)));
+       try {
+          //Date d= df.parse("2018-08-30 23:46:35");
+           System.out.println("今天的日期：" + df.format(d));
+           System.out.println("两天前的日期：" + df.format(new Date(d.getTime() -2 * 24 * 60 * 60 * 1000)));
+           System.out.println("40分钟后的日期：" + df.format(new Date(d.getTime() + 40 * 60 * 1000)));
+       }catch (Exception e){
+
+       }
     }
 }
